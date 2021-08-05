6c51c6c760c288ad2fd7eb5d81633764de1daf2b
dokeshraddha19 <shraddhadoke1999@gmail.com>
2021-08-06 00:20:18 +0530

    shr commit

---
+++ Line_UC1.java
@@ -0,0 +1,15 @@
+package computation;
+import java.util.*;
+public class Line_UC1 {
+		public static void main(String[] args) {
+			welcome ab=new welcome();
+			ab.method();
+			}
+		}
+	class welcome {
+			void method() {
+				System.out.println("Welcome to Line comparison problem");
+			}
+		}
+
+
